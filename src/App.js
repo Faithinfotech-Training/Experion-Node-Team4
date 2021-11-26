0b9c40b90c959fcf86f12f7847a7ca53f4352531
Sreyastp <sreyastp444@gmail.com>
2021-11-26 09:06:35 +0530

    updated pages

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Navbar,  Nav, NavDropdown} from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Header from './Header/Header';
+import Admin from './pages/Admin';
+import Routers from './Routers/Routers';
+
+export default function App() {
+return (
+	<>
+  <Header />
+  <Routers />
+  
+  </>
+);
+}
