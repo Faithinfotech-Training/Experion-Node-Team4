8af572d14b3a56ed15213545d9fcc35cc9231c23
Alwin-Sonny <alwin.sonny@experionglobal.com>
2021-11-26 14:52:11 +0530

    "modified login"

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Container from 'react-bootstrap/Container';
 import Header from './Header/Header';
 import Admin from './pages/Admin';
 import Routers from './Routers/Routers';
+import LoginForm from './pages/LoginForm';
 
 export default function App() {
 return (
 	<>
   <Header />
   <Routers />
-  
+
+  <LoginForm/>
   </>
 );
 }
