1ad8651a3912ddd0952e63fe18801b076140e991
nihalexperionglobal <95017803+nihalexperionglobal@users.noreply.github.com>
2021-11-26 17:29:46 +0530

    Delete routes directory

--- routes/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const staffRoutes = require('./staffs.route');
-
-router.use('/staffs', staffRoutes);
-module.exports = router;
