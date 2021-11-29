2849db6f6193e486e4251ae3af6f8ef1bf613054
sreyasilp <sreyas.tp@experionglobal.com>
2021-11-29 13:54:09 +0530

    Created Manager Router + pages

---
+++ src/pages/Manager/Manager.js
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
+
+import "./Manager.css";
+
+function Manager() {
+  return (
+    <div>
+      <div className="dashboard">
+        <div className="dashboarditem">Dashboard</div>
+        <div classname="dashboarditem">
+          <div className="item">
+            <NavDropdown title="View" id="basic-nav-dropdown">
+              <NavDropdown.Item>
+                <Link to="/manager/view-course-enquiry">
+                  {" "}
+                  View Course Enquiry{" "}
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/manager/view-resourse-enquiry">
+                  View Resource Enquiry
+                </Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </div>
+          <div className="item">
+            <NavDropdown title="Update Status" id="basic-nav-dropdown">
+              <NavDropdown.Item>
+                <Link to="/manager/course-enquiry-update-status">
+                  Course Enquiry Update Status
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                {" "}
+                <Link to="/manager/resource-enquiry-update-status">
+                  Resourse Enquiry Update Status
+                </Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </div>
+          <div className="item">
+            <NavDropdown title="Sales Pipeline" id="basic-nav-dropdown">
+              <NavDropdown.Item>
+                {" "}
+                <Link to="/manager/view-sales-pipeline">
+                  View Sales Pipeline
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>Dropdown </NavDropdown.Item>
+            </NavDropdown>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Manager;
---
+++ src/pages/Manager/ViewResourceEnquiry.js
@@ -0,0 +1,9 @@
+function ViewResourseEnquiry() {
+  return (
+    <div>
+      <p>This is page for View Resourse Enquiry</p>
+    </div>
+  );
+}
+
+export default ViewResourseEnquiry;
