497c2b69cea635fdc88f39dda5fc65e5fe0c61dd
Alwin-Sonny <alwin.sonny@experionglobal.com>
2021-11-29 09:22:54 +0530

    added Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import {Navbar,  Nav, NavDropdown} from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+// import './Header.css';
+
+
+
+function Home(){
+    return(
+<>
+<div className="navbarnfkt">
+	<div className="headerTitle"><div className="headerContent">Home Page</div></div>
+	<Navbar bg="primary" expand="lg" >
+  <Container>
+    <Navbar.Brand href="home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/regform">SignUp</Nav.Link>
+        <Nav.Link href="/loginform">Login</Nav.Link>
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+	</div>
+    
+</>    )
+}
+
+export default Home
