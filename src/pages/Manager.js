00d7e7e4df662e4043806ecd0769431860bdee8b
sreyasilp <sreyas.tp@experionglobal.com>
2021-11-29 11:46:00 +0530

    Created Manager Files

---
+++ src/pages/Manager.js
@@ -0,0 +1,36 @@
+import {Link} from "react-router-dom";
+import { NavDropdown } from 'react-bootstrap';
+
+import "./Admin.css";
+
+
+
+function Admin(){
+
+    return(
+        <div>
+        <div className="dashboard">
+            <div className="dashboarditem">Dashboard</div>
+            <div classname="dashboarditem">
+            <div className="item">
+                        <NavDropdown title="Course" id="basic-nav-dropdown">
+                            <NavDropdown.Item ><Link to="/admin/course">Course Management</Link></NavDropdown.Item>
+                            <NavDropdown.Item ><Link to="/admin/course-enquiry">Course Enquiry Management</Link></NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
+            <div className="item">
+            <NavDropdown title="Resource" id="basic-nav-dropdown"> 
+            <NavDropdown.Item >  < Link to="/admin/resource">Resource Management</Link></NavDropdown.Item>
+                            <NavDropdown.Item >Resource Enquiry Management</NavDropdown.Item>
+                            </NavDropdown>
+            </div> 
+
+            
+            </div>
+        </div>
+        </div>
+    )
+}
+
+
+export default Admin;
