1e6c0f24af6f4c30dbf7bfd1c7de936aa8270669
Anusha P R <anusha.pr@experionglobal.local>
2021-11-29 14:36:19 +0530

    added routes for resource enquiry management

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,33 +1,33 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { NavDropdown } from 'react-bootstrap';
 
 import "./Admin.css";
 
 
 
-function Admin(){
+function Admin() {
 
-    return(
+    return (
         <div>
-        <div className="dashboard">
-            <div className="dashboarditem">Dashboard</div>
-            <div classname="dashboarditem">
-            <div className="item">
+            <div className="dashboard">
+                <div className="dashboarditem">Dashboard</div>
+                <div classname="dashboarditem">
+                    <div className="item">
                         <NavDropdown title="Course" id="basic-nav-dropdown">
                             <NavDropdown.Item ><Link to="/admin/course">Course Management</Link></NavDropdown.Item>
                             <NavDropdown.Item ><Link to="/admin/course-enquiry">Course Enquiry Management</Link></NavDropdown.Item>
                         </NavDropdown>
                     </div>
-            <div className="item">
-            <NavDropdown title="Resource" id="basic-nav-dropdown"> 
-            <NavDropdown.Item >  < Link to="/admin/resource">Resource Management</Link></NavDropdown.Item>
-                            <NavDropdown.Item >Resource Enquiry Management</NavDropdown.Item>
-                            </NavDropdown>
-            </div> 
+                    <div className="item">
+                        <NavDropdown title="Resource" id="basic-nav-dropdown">
+                            <NavDropdown.Item > < Link to="/admin/resource">Resource Management</Link></NavDropdown.Item>
+                            <NavDropdown.Item ><Link to="/admin/resource-enquiry">Resource Enquiry Management</Link></NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
 
-            
+
+                </div>
             </div>
-        </div>
         </div>
     )
 }
---
+++ src/pages/resource enquiry management/EditReq/EditReq.js
@@ -0,0 +1,14 @@
+
+
+
+
+function EditReq(){
+
+    return(
+        <>
+        This page is for Viewing Resource Enq List
+        </>
+    )
+}
+
+export default EditReq;
