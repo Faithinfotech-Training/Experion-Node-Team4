e44f664e6b246310dad8ecd1642b831bc2586c22
sreyasilp <sreyas.tp@experionglobal.com>
2021-11-30 08:52:58 +0530

    Error Corrected

--- src/App.js
+++ src/App.js
@@ -4,42 +4,10 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Routers from "./Routers/Routers";
 
-
-import Chart from "./pages/Manager/Chart/Chart";
-import Tables from "./pages/Manager/Table/Table";
-import Tabel from "./pages/Manager/Table/TableAxios";
-import Products from "./pages/Manager/Table/TableApi";
-
 export default function App() {
   return (
     <>
       <Routers />
-      <Chart />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <Tables />
-      <Products />
-      {/* <Tabel/> */}
 
       {/* <LoginForm/> */}
     </>
