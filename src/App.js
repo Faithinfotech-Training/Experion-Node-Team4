653034db870055d98df611cedf1101bd52a849ad
sreyasilp <sreyas.tp@experionglobal.com>
2021-11-30 08:28:34 +0530

    save

--- src/App.js
+++ src/App.js
@@ -6,13 +6,41 @@ import Container from "react-bootstrap/Container";
 import Routers from "./Routers/Routers";
 import LoginForm from "./pages/LoginForm";
 
+import Chart from "./pages/Manager/Chart/Chart";
+import Tables from "./pages/Manager/Table/Table";
+import Tabel from "./pages/Manager/Table/TableAxios";
+import Products from "./pages/Manager/Table/TableApi";
 
 export default function App() {
   return (
     <>
-    
       <Routers />
-      
+      <Chart />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Tables />
+      <Products />
+      {/* <Tabel/> */}
 
       {/* <LoginForm/> */}
     </>
