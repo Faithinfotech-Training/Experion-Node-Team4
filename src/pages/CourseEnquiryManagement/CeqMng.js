7073ddd285ab01f064c1be87f6ae2f7f78b4f97a
NIHAL_FARHAN <nihal.kt@experionglobal.com>
2021-11-30 12:21:32 +0530

    Course Enquiry Status Update

--- src/pages/CourseEnquiryManagement/CeqMng.js
+++ src/pages/CourseEnquiryManagement/CeqMng.js
@@ -11,8 +11,8 @@ function CeqMng(){
          <Dashboard />
         
         <Link to="/admin/course-enquiry/view-course-enquiry">View Course Enquiries</Link>
-        <Link to="/admin/course-enquiry/edit-course-enquiry">Edit Course Enquiries</Link>
-        <Link to="/admin/course-enquiry/delete-course-enquiry">Delete Course Enquiries</Link>
+        {/* <Link to="/admin/course-enquiry/edit-course-enquiry">Edit Course Enquiries</Link>
+        <Link to="/admin/course-enquiry/delete-course-enquiry">Delete Course Enquiries</Link> */}
         </>
     )
 }
