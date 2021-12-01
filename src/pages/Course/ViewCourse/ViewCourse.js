0f6c73ca516f5cb2c0ea08a0b678fb77bd6c4559
NIHAL_FARHAN <nihal.kt@experionglobal.com>
2021-12-01 16:10:50 +0530

    Visit count for course page

--- src/pages/Course/ViewCourse/ViewCourse.js
+++ src/pages/Course/ViewCourse/ViewCourse.js
@@ -6,105 +6,80 @@ import { Link, useNavigate } from "react-router-dom";
 import Dashboard from "../../../Components/Dashboard/Dashboard";
 
 function ViewCourse() {
-  let [user, setUser] = useState(false);
+    let [user, setUser] = useState(false);
 
-  const [courses, setCourses] = useState([]);
+    const [courses, setCourses] = useState([]);
 
-  const [count, setCounter] = useState(0);
 
-  const navigate = useNavigate();
 
-  //   useEffect(() => {
-  //     console.log("The use effect hook has been executed");
+    const navigate = useNavigate();
 
-  //     var data = "";
 
-  //     // var config = {
-  //     //   method: "get",
-  //     //   url: "http://localhost:3009/courses",
+    const Users = () => {
+        if (localStorage.getItem("role") === "User") {
+            setUser(true);
+        }
+    };
+    let inc = 0;
+    const updateVisitCount = (id) => {
+        axios.get(`http://localhost:3009/courses/${id}`).then((res) => {
+            //   setCounter(response.data.visit + 1);
+            console.log('initial visit', res.data.visit);
+            let x = res.data.visit + 1;
+            axios
+                .put(`http://localhost:3009/courses/${id}`, { visit: x })
+                .then((response) => {
+                    console.log('Updated count', inc);
+                });
+        });
+    };
 
-  //     //   data: data,
-  //     // };
+    useEffect(() => {
+        console.log("The use effect hook has been executed");
 
-  //     axios.get.then((response) => {
-  //       console.log("Promise fulfilled");
-  //       console.log(response);
-  //       //response object contains the complete HTTP REQUEST with
-  //       //returned data, status code, and headers. We need only 'data'
-  //       setCourses(response.data);
-  //     });
-  //   }, []);
-  const Users = () => {
-    if (localStorage.getItem("role") === "User") {
-      setUser(true);
-    }
-  };
+        axios.get("http://localhost:3009/courses").then((response) => {
+            console.log("Promise fulfilled");
+            console.log(response);
+            //response object contains the complete HTTP REQUEST with
+            //returned data, status code, and headers. We need only 'data'
+            setCourses(response.data);
+        });
+        Users();
+    }, []);
 
-  const getVisitCount = (id) => {
-    axios.get(`http://localhost:3009/courses/${id}`).then((response) => {
-      setCounter(response.data.visit + 1);
-      console.log(response.data.visit);
-    });
-  };
-  const putVisitCount = (id) => {
-    axios
-      .put(`http://localhost:3009/courses/${id}`, { visit: count })
-      .then((response) => {
-        console.log(response.data);
-      });
-  };
+    return (
+        <div>
+            <Dashboard />
+            <div className="mainBody">
+                <h1>Course List</h1>
+                <div className="mainBox">
+                    {courses.map((course) => (
 
-  useEffect(() => {
-    console.log("The use effect hook has been executed");
+                        <div className="ULbox">
+                            <div>Course code: {course.coursecode}</div>
+                            <div>Course name: {course.coursename}</div>
 
-    axios.get("http://localhost:3009/courses").then((response) => {
-      console.log("Promise fulfilled");
-      console.log(response);
-      //response object contains the complete HTTP REQUEST with
-      //returned data, status code, and headers. We need only 'data'
-      setCourses(response.data);
-    });
-    Users();
-  }, []);
+                            {user ? (
+                                <button
+                                    onClick={async () => {
+                                        await updateVisitCount(course.id);
 
-  return (
-    <div>
-      <Dashboard />
-      <div className="mainBody">
-        <h1>Course List</h1>
-        <div className="mainBox">
-          {courses.map((course) => (
-            // <div key={staff.id} className="staffListLI">
-            //     {/* <Staff details = {staff} /> */}
-            // </div>
-            <div className="ULbox">
-              <div>Course code: {course.coursecode}</div>
-              <div>Course name: {course.coursename}</div>
-              {/* <div>
-                  <Link to={`${course.id}`}>View Course Details</Link>
-                </div> */}
-              {user ? (
-                <button
-                  onClick={async () => {
-                    // setCounter(count + 1);
-                    await getVisitCount(course.id);
-                    await putVisitCount(course.id);
-                    navigate(`/user/view-courses/${course.id}`);
-                  }}
-                >
-                  {count}
-                </button>
-              ) : (
-                <div>
-                  <Link to={`${course.id}`}>View Course Details</Link>
+                                        navigate(`/user/view-courses/${course.id}`);
+                                    }}
+                                >
+                                    Click for details
+                                </button>
+                            ) : (
+                                <div>
+                                    <Link to={`${course.id}`}>View Course Details</Link>
+                                </div>
+                            )}
+                        </div>
+                    ))}
                 </div>
-              )}
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ViewCourse;
