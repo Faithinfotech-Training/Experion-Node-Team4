258854a1cedaf9e4f74e402f393fc73d30b0166a
sreyasilp <sreyas.tp@experionglobal.com>
2021-12-01 18:18:42 +0530

    Table with Api Created

--- src/pages/Manager/SiteVIsitsTableVIew.js
+++ src/pages/Manager/SiteVIsitsTableVIew.js
@@ -1,12 +1,14 @@
 import ManagerDashboard from "../../Components/ManagerDashboard/ManagerDashboard";
 import Tables from "./Table/Table";
+import VisitTable from "./Table/TableAxios";
 
 function SitesVisitsTableView() {
   return (
     <div>
       <ManagerDashboard />
       {/* <p>This is page for Table...</p> */}
-      <Tables />
+      {/* <Tables /> */}
+      <VisitTable />
     </div>
   );
 }
