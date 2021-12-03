fa7c18cb9752da104e3ec8fce1ec8d6ffa2c0314
ALWIN <alwinaadhapally@gmail.com>
2021-12-03 14:18:19 +0530

    "list"

--- src/pages/Manager/Table/TableAxios.js
+++ src/pages/Manager/Table/TableAxios.js
@@ -1,108 +1,108 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { withStyles, makeStyles } from "@material-ui/core/styles";
+// import Table from "@material-ui/core/Table";
+// import TableBody from "@material-ui/core/TableBody";
+// import TableCell from "@material-ui/core/TableCell";
+// import TableContainer from "@material-ui/core/TableContainer";
+// import TableHead from "@material-ui/core/TableHead";
+// import TableRow from "@material-ui/core/TableRow";
+// import Paper from "@material-ui/core/Paper";
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
+// const StyledTableCell = withStyles((theme) => ({
+//   head: {
+//     backgroundColor: theme.palette.common.black,
+//     color: theme.palette.common.white,
+//   },
+//   body: {
+//     fontSize: 14,
+//   },
+// }))(TableCell);
 
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
+// const StyledTableRow = withStyles((theme) => ({
+//   root: {
+//     "&:nth-of-type(odd)": {
+//       backgroundColor: theme.palette.action.hover,
+//     },
+//   },
+// }))(TableRow);
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
+// const useStyles = makeStyles({
+//   table: {
+//     minWidth: 700,
+//   },
+// });
 
-const VisitTable = () => {
-  const classes = useStyles();
-  const [visit, setVisit] = useState([]);
-  const [search, setSearch] = useState("");
+// const VisitTable = () => {
+//   const classes = useStyles();
+//   const [visit, setVisit] = useState([]);
+//   const [search, setSearch] = useState("");
 
-  const getVisitData = async () => {
-    try {
-      const data = await axios.get(
-        "https://mocki.io/v1/09d3f919-7991-4a80-bdab-0143d0b6cb5c"
-      );
-      console.log(data.data);
-      setVisit(data.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+//   const getVisitData = async () => {
+//     try {
+//       const data = await axios.get(
+//         "https://mocki.io/v1/09d3f919-7991-4a80-bdab-0143d0b6cb5c"
+//       );
+//       console.log(data.data);
+//       setVisit(data.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
 
-  useEffect(() => {
-    getVisitData();
-  }, []);
-  return (
-    <div className="App">
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>No:</StyledTableCell>
-              <StyledTableCell align="left">
-                User &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <input
-                  type="text"
-                  align="center"
-                  placeholder="Search here"
-                  onChange={(e) => {
-                    setSearch(e.target.value);
-                  }}
-                />{" "}
-              </StyledTableCell>
-              <StyledTableCell align="left">Page</StyledTableCell>
-              <StyledTableCell align="left">Count</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {visit
-              .filter((item) => {
-                if (search == "") {
-                  return item;
-                } else if (
-                  item.name.toLowerCase().includes(search.toLowerCase())
-                ) {
-                  return item;
-                }
-              })
-              .map((item) => {
-                return (
-                  <StyledTableRow key={item.id}>
-                    <StyledTableCell component="th" scope="row">
-                      {item.id}
-                    </StyledTableCell>
-                    <StyledTableCell align="left">{item.user}</StyledTableCell>
-                    <StyledTableCell align="left">{item.page}</StyledTableCell>
-                    <StyledTableCell align="left">{item.count}</StyledTableCell>
-                  </StyledTableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
-};
+//   useEffect(() => {
+//     getVisitData();
+//   }, []);
+//   return (
+//     <div className="App">
+//       <TableContainer component={Paper}>
+//         <Table className={classes.table} aria-label="customized table">
+//           <TableHead>
+//             <TableRow>
+//               <StyledTableCell>No:</StyledTableCell>
+//               <StyledTableCell align="left">
+//                 User &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+//                 <input
+//                   type="text"
+//                   align="center"
+//                   placeholder="Search here"
+//                   onChange={(e) => {
+//                     setSearch(e.target.value);
+//                   }}
+//                 />{" "}
+//               </StyledTableCell>
+//               <StyledTableCell align="left">Page</StyledTableCell>
+//               <StyledTableCell align="left">Count</StyledTableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {visit
+//               .filter((item) => {
+//                 if (search == "") {
+//                   return item;
+//                 } else if (
+//                   item.name.toLowerCase().includes(search.toLowerCase())
+//                 ) {
+//                   return item;
+//                 }
+//               })
+//               .map((item) => {
+//                 return (
+//                   <StyledTableRow key={item.id}>
+//                     <StyledTableCell component="th" scope="row">
+//                       {item.id}
+//                     </StyledTableCell>
+//                     <StyledTableCell align="left">{item.user}</StyledTableCell>
+//                     <StyledTableCell align="left">{item.page}</StyledTableCell>
+//                     <StyledTableCell align="left">{item.count}</StyledTableCell>
+//                   </StyledTableRow>
+//                 );
+//               })}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//     </div>
+//   );
+// };
 
-export default VisitTable;
+// export default VisitTable;
