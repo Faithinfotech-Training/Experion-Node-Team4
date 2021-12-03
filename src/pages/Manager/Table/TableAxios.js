569cec2475d2f82a87bdd31d0922f508fdb55789
sreyasilp <sreyas.tp@experionglobal.com>
2021-12-03 17:03:36 +0530

    All chart and table cleared

--- src/pages/Manager/Table/TableAxios.js
+++ src/pages/Manager/Table/TableAxios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
+import "./Table.css";
 
 const VisitTable = () => {
   const [visit, setVisit] = useState([]);
@@ -23,48 +24,107 @@ const VisitTable = () => {
   }, []);
 
   return (
-    <div className="App">
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>No:</th>
-            <th align="left">
-              User
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <input
-                type="text"
-                align="center"
-                placeholder="Search here"
-                onChange={(e) => {
-                  setSearch(e.target.value);
-                }}
-              />
-            </th>
-            <th align="left">Page</th>
-            <th align="left">Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {visit
-            .filter((item) => {
-              if (search == "") {
-                return item;
-              } else if (item.user.includes(search)) {
-                return item;
-              }
-            })
-            .map((item) => {
-              return (
-                <tr key={item.id}>
-                  <td component="th">{item.id}</td>
-                  <td align="left">{item.user}</td>
-                  <td align="left">{item.page}</td>
-                  <td align="left">{item.count}</td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </Table>
+    <div className="tablebox">
+      <div className="container-fluid ">
+        {/* <h3 className={styles.headerText}>Dashboard</h3> */}
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 p-1">
+            <div className="card shadow mb-xl-2 mb-lg-2 mg-md-2 mb-sm-2 mb-2">
+              <h4>Course Visit</h4>
+
+              <Table striped bordered hover size="sm">
+                <thead>
+                  <tr>
+                    <th>No:</th>
+                    <th align="left">
+                      User
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <input
+                        type="text"
+                        align="center"
+                        placeholder="Search here"
+                        onChange={(e) => {
+                          setSearch(e.target.value);
+                        }}
+                      />
+                    </th>
+                    <th align="left">Page</th>
+                    <th align="left">Count</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {visit
+                    .filter((item) => {
+                      if (search == "") {
+                        return item;
+                      } else if (item.user.includes(search)) {
+                        return item;
+                      }
+                    })
+                    .map((item) => {
+                      return (
+                        <tr key={item.id}>
+                          <td component="th">{item.id}</td>
+                          <td align="left">{item.user}</td>
+                          <td align="left">{item.page}</td>
+                          <td align="left">{item.count}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 p-1">
+            <div className="card shadow mb-xl-2 mb-lg-2 mg-md-2 mb-sm-2 mb-2">
+              <h4>Resource Visit</h4>
+
+              <Table striped bordered hover size="sm">
+                <thead>
+                  <tr>
+                    <th>No:</th>
+                    <th align="left">
+                      User
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <input
+                        type="text"
+                        align="center"
+                        placeholder="Search here"
+                        onChange={(e) => {
+                          setSearch(e.target.value);
+                        }}
+                      />
+                    </th>
+                    <th align="left">Page</th>
+                    <th align="left">Count</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {visit
+                    .filter((item) => {
+                      if (search == "") {
+                        return item;
+                      } else if (item.user.includes(search)) {
+                        return item;
+                      }
+                    })
+                    .map((item) => {
+                      return (
+                        <tr key={item.id}>
+                          <td component="th">{item.id}</td>
+                          <td align="left">{item.user}</td>
+                          <td align="left">{item.page}</td>
+                          <td align="left">{item.count}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
     </div>
   );
 };
