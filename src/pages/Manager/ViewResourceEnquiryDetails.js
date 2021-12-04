4b9e22fb1de3c467735f77dac46cc93a64bca3a2
ALWIN <alwinaadhapally@gmail.com>
2021-12-04 10:53:59 +0530

    "added table structure for manager views"

--- src/pages/Manager/ViewResourceEnquiryDetails.js
+++ src/pages/Manager/ViewResourceEnquiryDetails.js
@@ -4,7 +4,7 @@ import ManagerDashboard from "../../Components/ManagerDashboard/ManagerDashboard
 import axios from "axios";
 import {useState, useEffect} from "react";
 import { useParams } from "react-router";
-
+import { Table } from "react-bootstrap";
 
 function ManagerViewResourceEnquiryDetails(){
 
@@ -32,11 +32,28 @@ function ManagerViewResourceEnquiryDetails(){
     return(
         <>
         <ManagerDashboard />
-        <h1>Resource enquiry Details</h1>
-        <h2>Enquirer name: {inputs.name}</h2>
-        <h2>Enquirer email : { inputs.email}</h2>
-        <h2>Enquired resource: {inputs.resourceName}</h2>
-
+        <div className="resource-list">
+      <center>  <h1>Resource enquiry Details</h1> </center>
+      
+      <Table striped bordered>
+      <thead>
+      <tr>
+   
+   <th> Enquirer Name</th>
+   <th> Enquirer Email</th>
+   <th> Enquired resource</th>
+</tr></thead>
+<tbody>
+<tr>
+      <td> {inputs.name}</td>
+      <td> {inputs.email}</td>
+      
+      <td> {inputs.resourceName}</td>
+      </tr>
+      </tbody>
+      
+      </Table>
+         </div>
         </>
     )
 }
