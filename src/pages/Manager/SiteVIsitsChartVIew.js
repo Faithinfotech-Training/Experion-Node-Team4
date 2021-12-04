6e1468fc2955f37c6215968ecf82c2b27873e684
sreyasilp <sreyas.tp@experionglobal.com>
2021-12-04 13:05:49 +0530

    Chart Resourse Set

--- src/pages/Manager/SiteVIsitsChartVIew.js
+++ src/pages/Manager/SiteVIsitsChartVIew.js
@@ -20,13 +20,13 @@ function SitesVisitsChartView() {
         <div className="row">
           <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 p-1">
             <div className="card shadow mb-xl-2 mb-lg-2 mg-md-2 mb-sm-2 mb-2">
-              <h4>Pie Chart</h4>
+              <h4>Course Chart</h4>
               <PieChart />
             </div>
           </div>
           <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 p-1">
             <div className="card shadow mb-xl-2 mb-lg-2 mg-md-2 mb-sm-2 mb-2">
-              <h4>Donought Chart</h4>
+              <h4>Resourse Chart</h4>
               <Chart />
             </div>
           </div>
